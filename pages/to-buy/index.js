600fe3006af3fc78fcd50a5adffe176d469ac0a4
Настя <smelw@list.ru>
2022-07-05 18:56:12 +0300

    first start

---
+++ pages/to-buy/index.js
@@ -0,0 +1,15 @@
+const ToBuy = () => {
+    return (
+      <div>
+        <h1>видос</h1>
+        <h1>видос</h1>
+        <h1>видос</h1>
+        <h1>видос</h1>
+        <h1>видос</h1>
+        <h1>видос</h1>
+      </div>
+    );
+  };
+  
+  export default ToBuy;
+  
---
+++ pages/to-watch/index.js
@@ -0,0 +1,14 @@
+const ToWatch = () => {
+  return (
+    <div>
+      <h1>видос</h1>
+      <h1>видос</h1>
+      <h1>видос</h1>
+      <h1>видос</h1>
+      <h1>видос</h1>
+      <h1>видос</h1>
+    </div>
+  );
+};
+
+export default ToWatch;
