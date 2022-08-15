0cab33c9b1d3f942e1929ecf54e294dade29583f
Настя <smelw@list.ru>
2022-08-16 00:59:50 +0300

    changed todo

--- pages/to-watch/index.js
+++ pages/to-watch/index.js
@@ -1,57 +1,71 @@
+import { useEffect } from "react";
 import MainLayout from "../../components/MainLayout";
 import MainTodo from "../../components/Todo";
 
-export async function getStaticProps() {
-  return {
-    props: {
-      movies: [
-        {
-          id: 1,
-          text: "Игра Джеральда",
-          isChecked: false,
-        },
-        {
-          id: 2,
-          text: "Век Адалин",
-          isChecked: false,
-        },
-        {
-          id: 3,
-          text: "Толкин",
-          isChecked: false,
-        },
-        {
-          id: 4,
-          text: "Мгла",
-          isChecked: false,
-        },
-        {
-          id: 5,
-          text: "Пролетая над гнездом кукушки",
-          isChecked: false,
-        },
-        {
-          id: 6,
-          text: "Бегущий по лезвию",
-          isChecked: false,
-        },
-        {
-          id: 7,
-          text: "Сияние",
-          isChecked: false,
-        },
-      ],
-      title: "Movies",
-    },
-  };
-}
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       movies: [
+//         {
+//           id: 1,
+//           text: "Игра Джеральда",
+//           isChecked: false,
+//         },
+//         {
+//           id: 2,
+//           text: "Век Адалин",
+//           isChecked: false,
+//         },
+//         {
+//           id: 3,
+//           text: "Толкин",
+//           isChecked: false,
+//         },
+//         {
+//           id: 4,
+//           text: "Мгла",
+//           isChecked: false,
+//         },
+//         {
+//           id: 5,
+//           text: "Пролетая над гнездом кукушки",
+//           isChecked: false,
+//         },
+//         {
+//           id: 6,
+//           text: "Бегущий по лезвию",
+//           isChecked: false,
+//         },
+//         {
+//           id: 7,
+//           text: "Сияние",
+//           isChecked: false,
+//         },
+//       ],
+//       title: "Movies",
+//     },
+//   };
+// }
 
-const Movies = ({ movies }) => {
+const Movies = ({ todos }) => {
+
   return (
     <MainLayout>
-      <MainTodo todoListStatic={movies} />
+      <MainTodo todoListStatic={todos} />
     </MainLayout>
   );
 };
+
+export async function getServerSideProps(context) {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await response.json();
+
+  return {
+    props: {
+      todos: data.slice(0, 20),
+      title: "Todos",
+    },
+  };
+}
 
 export default Movies;
